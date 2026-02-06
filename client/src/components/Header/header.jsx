31bdcd8a3da4c82bd0b5e20e9ddf5a2dda6f7c9e
Angelo Garcia <garciaangelo110996@gmail.com>
2026-02-07 06:56:41 +0800

    Done: Home page section Next:Shopping Section

--- client/src/components/Header/header.jsx
+++ client/src/components/Header/header.jsx
@@ -1,3 +1,4 @@
+import {ShoppingCartIcon} from '@heroicons/react/24/outline';
 import HomeSection from "../../pages/Home";
 import ShopSection from "../../pages/Shop";
 import ContactSection from "../../pages/Contact";
@@ -7,22 +8,33 @@ export default function HeaderSection(){
     return(
         <section className="scroll-smooth">
   <div className="container mx-auto sm:px-6 md:px-10 lg:px-16">
-    <nav className="fixed px-16 bg-sky-100 top-0 left-0 right-10 w-full h-16 grid grid-cols-3 items-center ">
-      <div>LOGO</div>
+    <nav className="fixed px-16 bg-white top-0 left-0 right-10 w-full h-16 grid grid-cols-3 items-center ">
+     <div
+  className="h-15 w-40 bg-no-repeat bg-contain bg-center"
+  style={{
+    backgroundImage: `url("https://res.cloudinary.com/dl1oaa2td/image/upload/v1770411099/GeloExpressLogo_cktzjw.png")`
+  }}
+></div>
+
       <div className="flex flex-wrap gap-6 md:gap-20 justify-center">
         <a href="#home">Home</a>
         <a href="#shop">Shop</a>
         <a href="#about">About</a>
         <a href="#contact">Contact</a>
       </div>
-      <div className="flex justify-end gap-2">
-        <button className="px-4 py-2 bg-amber-300 rounded">Login</button>
-        <button className="px-4 py-2 bg-amber-300 rounded">Sign-up</button>
+      <div className="flex justify-end gap-5 items-center">
+        
+        <ShoppingCartIcon className='h-6 w-6 hover:text-amber-500' />
+        <div className='flex gap-2'>
+          <button className="px-4 py-2 bg-green-400 hover:bg-green-600 rounded text-white">Login</button>
+          <button className="px-4 py-2 text-blue-500 rounded  hover:bg-blue-600 hover:text-white font-normal ">Sign-up</button>
+        </div>
+        
       </div>
     </nav>
 
         <div className="pt-16">
-                  <HomeSection />
+                <HomeSection />
                 <ShopSection />
                 <AboutSection />
                 <ContactSection />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,72 @@
+import { useEffect,useState } from "react"
+
 export default function HomeSection(){
-    return(
-        <section id="home" className="min-h-screen bg-amber-50">
-               <h1>this is home</h1>
-        </section>
-    )
+
+    const homeImage = [
+        {
+            imageUrl: "https://res.cloudinary.com/dl1oaa2td/image/upload/v1770413789/HomeImage1_ips3lp.jpg",
+            bigText:"Wear the Season. Feel the Moment",
+            longText:"Designed with lightweight, breathable fabric, our summer shirts keep you cool and effortlessly stylish all day long. Made for couples who love comfort and connection, this collection lets you create memories in style."
+
+        },
+        {
+            imageUrl: "https://res.cloudinary.com/dl1oaa2td/image/upload/v1770406386/HomeImage3_qnzfes.jpg",
+            bigText:"Smart Living. Simplified",
+            longText:"Our modern smartwatch combines sleek design with powerful features to keep you connected and in control. Track your health, manage your day, and stay ahead — all from your wrist."
+
+        },
+        
+        {
+            imageUrl: "https://res.cloudinary.com/dl1oaa2td/image/upload/v1770406387/HomeImage2_vu07mh.jpg",
+            bigText:"Details That Define You",
+            longText:"Crafted from premium materials, our classic shoes and leather belts bring timeless elegance to any outfit. Every detail is designed to elevate your confidence and complete your look with sophistication."
+
+        },
+        
+    ]
+    
+    const[ currentIndex , setCurrentIndex ]=useState(0);
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            setCurrentIndex((prev)=>
+                prev === homeImage.length - 1 ? 0 : prev + 1
+            );
+        },4000)
+        return ()=> clearInterval(interval)
+    },[homeImage.length] )
+
+        return(
+            <section id="home" className="min-h-screen">
+                
+                <div className="relative h-screen overflow-hidden">
+                    {homeImage.map((item,index)=>(
+                        <div
+                        key={index}
+                        className={`absolute inset-0 h-screen bg-cover bg-center transition-opacity duration-1000 ${
+                            index === currentIndex ? "opacity-100" : "opacity-0"
+                        }`}
+                        style={{ backgroundImage: `url(${item.imageUrl})` }}
+                        >
+                            <div className="flex flex-col h-[70vh] gap-3">
+                                <div className="mt-auto">
+                                    <span className="ml-5 text-7xl text-white font-extrabold">
+                                    {item.bigText}
+                                    </span>
+                                   
+                                </div>
+                                <div className="w-[100vh] flex">
+                                    <span className="ml-5 text-xl text-white font-light">
+                                        {item.longText}
+                                    </span>
+                                </div>
+                            </div>
+
+                        </div>
+                    ))}
+                </div>
+                
+                
+            </section>
+        )
 }
